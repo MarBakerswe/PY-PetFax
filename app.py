50f3d87dfa1cbe9960c98f4136d651a7f50e050a
Marlon Baker <marbaker.swe@gmail.com>
2023-05-10 14:53:29 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from petfax import create_app
+app = create_app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# config
+# from flask import Flask
+# app = Flask(__name__)
+
+# # index rout
+
+# @app.route('/')
+# def index():
+#     return 'Hello, this is PetFax!'
+
+# #pets route
+
+# @app.route('/pets')
+# def pets():
+#     return 'These are our pets please come get these motherfuckers!!'
